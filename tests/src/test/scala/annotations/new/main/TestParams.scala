2fdca356ec18bb941696692a6a9769e5dfb87520
dveim <alexandrolgash@gmail.com>
2016-09-23 17:11:31 +0300

    tests are green == #37 in fixed

--- tests/src/test/scala/annotations/new/main/TestParams.scala
+++ tests/src/test/scala/annotations/new/main/TestParams.scala
@@ -1,5 +1,7 @@
 package mainWithParams
 
+import scala.meta._
+
 @mainWithParams("hello world")
 object Test {
   println("bye-bye world")
@@ -10,3 +12,10 @@ class SomeClass1
 
 @argRepeated(foos: _*)
 class SomeClass2
+
+@argRepeated(foos: _*)
+class SomeClass3 {
+  @Deprecated inline implicit def foo(a: Int, b: Int): Any = meta {
+    a + b
+  }
+}
